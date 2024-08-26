32d06bc52a8d632c1969a648acf8d523f6ba2081
TeamMeng <862915265@qq.com>
2024-08-26 22:34:48 +0800

    feature: add csv cli support

---
+++ src/process.rs
@@ -0,0 +1,26 @@
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player {
+    name: String,
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+}
+
+pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
+    let mut reader = Reader::from_path(input)?;
+    let mut ret = Vec::new();
+    for result in reader.deserialize::<Player>() {
+        ret.push(result?);
+    }
+    let json = serde_json::to_string_pretty(&ret)?;
+    fs::write(output, json)?;
+    Ok(())
+}
